eba67f9ef550126acb48bf4a63ce00445cb5dbe6
Christopher Finster <finsternavy@gmail.com>
2022-04-08 15:30:07 -0500

    103_competency_report added comments
    added comments to JS

--- toDo.js
+++ toDo.js
@@ -1,15 +1,26 @@
 let task = "";
 let taskArray = new Array();
 
+// add a task to the task array
 function addTask(){
+    //get the task from the user
     task = prompt("Enter a task: ");
+    // only place the task in the array if a task is actually typed
     if(task != "" && task != null){
+        // add the task
         taskArray.push(task);
+        // keep track and verify
         console.log(taskArray);
     }
     
+    // local variable to clear value automatically after each task creation
     let innerHTMLText = "";
+
+    // iterated depending on number of elements in the array
     for(let i = 0; i < taskArray.length; i++){
+
+        // add the html for each element into a single string
+        // also dynamically creates delete button for each task
         innerHTMLText = innerHTMLText + `<div class="task-container"><button onclick="removeTask(${i});" class="list-button">X</button><li class="list-item">${taskArray[i]}</li></div>`;
     }
 
@@ -19,12 +30,13 @@ function addTask(){
 }
 
 function removeTask(taskIndex){
+    // remove the specific element from the array
     taskArray.splice(taskIndex, 1);
     
     console.log(taskArray);
 
     let innerHTMLText = "";
-    
+
     for(let i = 0; i < taskArray.length; i++){
         innerHTMLText = innerHTMLText + `<div class="task-container"><button onclick="removeTask(${i});" class="list-button">X</button><li class="list-item">${taskArray[i]}</li></div>`;
     }
